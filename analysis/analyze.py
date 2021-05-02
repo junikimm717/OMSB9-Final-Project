e440ff1a5771ae4b5274c57bba2600f04c59109a
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2021-05-01 20:06:13 -0400

    Writing Paper

--- analysis/analyze.py
+++ analysis/analyze.py
@@ -5,11 +5,12 @@ import scipy.stats as s
 import numpy as np
 import matplotlib.pyplot as plt
 from sys import argv
+import math
 
 
 def test_1(plot=False) -> None:
     x = np.array([x.correct for x in p.results])
-    y = np.array([abs(x.response - x.correct) for x in p.results])
+    y = np.array([(abs(x.response - x.correct)) for x in p.results])
     means = np.array([np.std([
         (x.response - x.correct) for x in p.results if x.correct == i
     ]) for i in range(1, 10)])
@@ -19,16 +20,16 @@ def test_1(plot=False) -> None:
         slope, intercept, rvalue, pvalue, stderr = regress
         y_p = intercept + slope * x
         plt.plot(x, y_p, color='green')
-        plt.title("Test 1")
-        plt.xlabel('Number displayed on screen')
-        plt.ylabel('Standard Deviation from answer')
+        plt.title("Deviations from Answer based on number displayed")
+        plt.xlabel('Number Displayed on Screen')
+        plt.ylabel('Standard Deviation From Answer')
         #plt.scatter(x, y)
         plt.scatter(range(1, 10), means, color='red')
-        plt.show()
+        plt.savefig('../paper/images/regression.jpg')
+        #plt.show()
 
     else:
         print("\n\nresults 1...")
-        # anything relating to deviations by number.
         print(regress)
         print(s.spearmanr(range(1, 10), means))
 
@@ -42,7 +43,7 @@ def test_2(plot=False) -> None:
                     res += 1
             return res
 
-        plt.title("Frequencies of margin of error across reaction times.")
+        plt.title("Frequencies of Margin of Error Across Reaction Times")
         data = [[abs(x.response - x.correct) for x in p.results if x.time == i]
                 for i in [250, 500, 750, 1000]]
 
@@ -51,9 +52,13 @@ def test_2(plot=False) -> None:
             lt = data[i]
             time = 250 * (i + 1)
             f = [freq(lt, j) for j in range(5)]
-            plt.scatter(range(5), f, color=colors[i], label=f'Frequencies with {time}ms')
+            plt.scatter(range(5), f, color=colors[i], label=f'{time}ms')
+
+        plt.xlabel("Amount missed")
+        plt.ylabel("Frequency")
         plt.legend(loc='upper right')
-        plt.show()
+        plt.savefig('../paper/images/reaction.jpg')
+        #plt.show()
     else:
         print("\n\nresults 2...")
         deviations = [abs(x.response - x.correct) for x in p.results]
@@ -76,19 +81,46 @@ def test_4() -> None:
     print(s.ttest_ind(deviations[0], deviations[1], alternative='less', equal_var=False))
 
 
+def test_5(plot=False) -> None:
+    if plot:
+        def freq(ltd: list, el: object) -> int:
+            res = 0
+            for x in ltd:
+                if el == x:
+                    res += 1
+            return res
+
+        plt.title("Frequencies of Margin of Error Across Peripheral Vision Angle")
+        data = [[abs(x.response - x.correct) for x in p.results if x.angle == i]
+                for i in [0, 30, 60]]
+
+        colors = ['blue', 'green', 'red', 'yellow']
+        for i in range(len(data)):
+            lt = data[i]
+            angle = 30*i
+            f = [freq(lt, j) for j in range(5)]
+            plt.scatter(range(5), f, color=colors[i], label=f'{angle} degrees')
+
+        plt.xlabel("Amount missed")
+        plt.ylabel("Frequency")
+        plt.legend(loc='upper right')
+        plt.savefig('../paper/images/angle.jpg')
+        #plt.show()
+
+    else:
+        pass
+
 def main():
     if len(argv) == 1:
         print("not enough args.")
         print("Format is (function to test) (boolean for whether or not you want a plot.)")
         exit(1)
-    functions = [None, test_1, test_2, test_3, test_4]
-    try:
-        if len(argv) == 3:
-            functions[int(argv[1])](bool(argv[2]))
-        elif len(argv) == 2:
-            functions[int(argv[1])]()
-    except Exception:
-        print("Error. Format is (function to test) (boolean for whether or not you want a plot.)")
+    functions = [None, test_1, test_2, test_3, test_4, test_5]
+
+    if len(argv) == 3:
+        functions[int(argv[1])](bool(argv[2]))
+    elif len(argv) == 2:
+        functions[int(argv[1])]()
 
 
 if __name__ == "__main__":
