bbfb765cc1caa689c29621ab13121c62ce08dfde
junikimm717 <68165832+junikimm717@users.noreply.github.com>
2021-07-19 13:38:08 -0400

    depythontexed

--- paper/build.sh
+++ paper/build.sh
@@ -3,9 +3,11 @@
 case $OSTYPE in
     linux*)
         pythontex $1
+        depythontex $1
     ;;
     darwin*)
         # stupid darwin defaulting to python 2
+        /usr/local/texlive/2021/texmf-dist/scripts/pythontex/depythontex3.py $1
         /usr/local/texlive/2021/texmf-dist/scripts/pythontex/pythontex3.py $1
     ;;
 esac
